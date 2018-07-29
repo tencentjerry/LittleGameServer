36b9b9e4da4c57a3c56be4dbae9859e44becd36e
tencentjerry <95728279@qq.com>
2018-07-29 20:31:19 +0800

    lib

---
+++ lib/type_define.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace lib
+{
+
+using int8 = char;
+using uint8 = unsigned char;
+using int16 = short;
+using uint16 = unsigned short;
+using int32 = int;
+using uint32 = unsigned int;
+using int64 = long;
+using uint64 = unsigned long;
+
+}
